78a83e5012e088dba7b2ad41ec981bb7d0378384
Leonardo <leonardo38361@gmail.com>
2023-08-07 04:04:18 -0300

    single responsability principle

--- scripts/passwordValidation.mjs
+++ scripts/passwordValidation.mjs
@@ -1,32 +1,49 @@
+const message = document.querySelector('#message');
+
+const validPasswordMessage = () => {
+	message.innerHTML = 'Contraseña válida';
+	message.style.color = 'green';
+	showWarningMessage(false);
+};
+
+const invalidPasswordMessage = () => {
+	message.innerHTML = 'La contraseña no cumple con los requisitos';
+	message.style.color = 'red';
+	showWarningMessage(true);
+};
+
 const passwordValidation = () => {
 	const password = document.querySelector('#fpassword').value;
-	const message = document.querySelector('#message');
 
 	const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$/;
 
 	if (regex.test(password)) {
-		message.innerHTML = 'Contraseña válida';
-		message.style.color = 'green';
-		showMessageWarning(false);
+		validPasswordMessage();
 	} else {
-		message.innerHTML = 'La contraseña no cumple con los requisitos';
-		message.style.color = 'red';
-		showMessageWarning(true);
+		invalidPasswordMessage();
 	}
 };
 
-function showMessageWarning(messageDisplayed) {
-	const button = document.querySelector('#button');
-	const warningTitle = document.querySelector('#warning-title');
+const button = document.querySelector('#button');
+const warningTitle = document.querySelector('#warning-title');
 
+const warningMessage = () => {
+	warningTitle.style.display = 'block';
+	warningTitle.textContent = `La contraseña debe tener almenos menos debe tener 1 mayúscula, al
+	menos 1 minúscula, al menos 1 dígito numérico, longitud entre 8 y 16
+	caracteres`;
+	button.disabled = true;
+};
+
+const warningMessageRemoved = () => {
+	warningTitle.style.display = 'none';
+	button.disabled = false;
+};
+
+function showWarningMessage(messageDisplayed) {
 	if (messageDisplayed === true) {
-		warningTitle.style.display = 'block';
-		warningTitle.textContent = `La contraseña debe tener almenos menos debe tener 1 mayúscula, al
-				menos 1 minúscula, al menos 1 dígito numérico, longitud entre 8 y 16
-				caracteres`;
-		button.disabled = true;
+		warningMessage();
 	} else {
-		warningTitle.style.display = 'none';
-		button.disabled = false;
+		warningMessageRemoved();
 	}
 }
