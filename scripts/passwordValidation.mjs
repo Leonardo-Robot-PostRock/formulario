dc3339458351ab9b87e65c53a8e4ea51e8b61c93
Leonardo <leonardo38361@gmail.com>
2023-08-07 03:45:05 -0300

    mejorado del código

--- scripts/passwordValidation.mjs
+++ scripts/passwordValidation.mjs
@@ -1,33 +1,32 @@
 const passwordValidation = () => {
 	const password = document.querySelector('#fpassword').value;
+	const message = document.querySelector('#message');
 
 	const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$/;
 
 	if (regex.test(password)) {
-		document.getElementById('message').innerHTML = 'Contraseña válida';
-		document.getElementById('message').style.color = 'green';
+		message.innerHTML = 'Contraseña válida';
+		message.style.color = 'green';
 		showMessageWarning(false);
 	} else {
-		document.getElementById('message').innerHTML =
-			'La contraseña no cumple con los requisitos';
-		document.getElementById('message').style.color = 'red';
+		message.innerHTML = 'La contraseña no cumple con los requisitos';
+		message.style.color = 'red';
 		showMessageWarning(true);
 	}
 };
 
 function showMessageWarning(messageDisplayed) {
 	const button = document.querySelector('#button');
+	const warningTitle = document.querySelector('#warning-title');
 
 	if (messageDisplayed === true) {
-		document.getElementById('warning-title').style.display = 'block';
-		document.getElementById(
-			'warning-title'
-		).textContent = `La contraseña debe tener almenos menos debe tener 1 mayúscula, al
+		warningTitle.style.display = 'block';
+		warningTitle.textContent = `La contraseña debe tener almenos menos debe tener 1 mayúscula, al
 				menos 1 minúscula, al menos 1 dígito numérico, longitud entre 8 y 16
 				caracteres`;
 		button.disabled = true;
 	} else {
-		document.getElementById('warning-title').style.display = 'none';
+		warningTitle.style.display = 'none';
 		button.disabled = false;
 	}
 }
