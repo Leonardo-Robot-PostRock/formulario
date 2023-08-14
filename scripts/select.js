20d9035ed91bcdb0091bc46d85be516e3f9cb513
Leonardo <leonardo38361@gmail.com>
2023-08-14 14:50:25 -0300

    se agrega clickReset para button limpiar, para reiniciar elemento select. Se mejora el código con single responsability principle

--- scripts/select.js
+++ scripts/select.js
@@ -1,83 +1,128 @@
-let x, i, j, l, ll, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName('custom-select');
-l = x.length;
-for (i = 0; i < l; i++) {
-	selElmnt = x[i].getElementsByTagName('select')[0];
-	ll = selElmnt.length;
-	/*for each element, create a new DIV that will act as the selected item:*/
-	a = document.createElement('DIV');
-	a.setAttribute('class', 'select-selected');
-	a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-	x[i].appendChild(a);
-	/*for each element, create a new DIV that will contain the option list:*/
-	b = document.createElement('DIV');
-	b.setAttribute('class', 'select-items select-hide');
-	for (j = 1; j < ll; j++) {
-		/*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
-		c = document.createElement('DIV');
-		c.innerHTML = selElmnt.options[j].innerHTML;
-		c.addEventListener('click', function (e) {
-			/*when an item is clicked, update the original select box,
-        and the selected item:*/
-			var y, i, k, s, h, sl, yl;
-			s = this.parentNode.parentNode.getElementsByTagName('select')[0];
-			sl = s.length;
-			h = this.parentNode.previousSibling;
-			for (i = 0; i < sl; i++) {
-				if (s.options[i].innerHTML == this.innerHTML) {
-					s.selectedIndex = i;
-					h.innerHTML = this.innerHTML;
-					y = this.parentNode.getElementsByClassName('same-as-selected');
-					yl = y.length;
-					for (k = 0; k < yl; k++) {
-						y[k].removeAttribute('class');
-					}
-					this.setAttribute('class', 'same-as-selected');
-					break;
-				}
-			}
-			h.click();
-		});
-		b.appendChild(c);
+// Initialize custom select elements on the page
+function initializeCustomSelects() {
+	const customSelects = document.getElementsByClassName('custom-select');
+	for (let i = 0; i < customSelects.length; i++) {
+		initializeSingleSelect(customSelects[i]);
 	}
-	x[i].appendChild(b);
-	a.addEventListener('click', function (e) {
-		/*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
-		e.stopPropagation();
-		closeAllSelect(this);
-		this.nextSibling.classList.toggle('select-hide');
-		this.classList.toggle('select-arrow-active');
+}
+
+// Initialize a single custom select element
+function initializeSingleSelect(selectContainer) {
+	const selectElement = selectContainer.querySelector('select');
+	const selectedDiv = createSelectedDiv(selectElement);
+	const optionContainer = createOptionContainer(selectElement);
+
+	selectContainer.appendChild(selectedDiv);
+	selectContainer.appendChild(optionContainer);
+
+	selectedDiv.addEventListener('click', function (event) {
+		event.stopPropagation();
+		closeAllSelectsExcept(selectedDiv);
+		toggleOptionContainer(optionContainer, selectedDiv);
 	});
 }
-function closeAllSelect(elmnt) {
-	/*a function that will close all select boxes in the document,
-  except the current select box:*/
-	var x,
-		y,
-		i,
-		xl,
-		yl,
-		arrNo = [];
-	x = document.getElementsByClassName('select-items');
-	y = document.getElementsByClassName('select-selected');
-	xl = x.length;
-	yl = y.length;
-	for (i = 0; i < yl; i++) {
-		if (elmnt == y[i]) {
-			arrNo.push(i);
-		} else {
-			y[i].classList.remove('select-arrow-active');
+
+// Create and configure the selected item div
+function createSelectedDiv(selectElement) {
+	const selectedDiv = document.createElement('DIV');
+	selectedDiv.setAttribute('class', 'select-selected');
+	selectedDiv.innerHTML =
+		selectElement.options[selectElement.selectedIndex].innerHTML;
+	return selectedDiv;
+}
+
+// Create and configure the options container div
+function createOptionContainer(selectElement) {
+	const optionContainer = document.createElement('DIV');
+	optionContainer.setAttribute('class', 'select-items select-hide');
+
+	for (let j = 1; j < selectElement.length; j++) {
+		const optionItem = createOptionItem(selectElement.options[j]);
+		optionContainer.appendChild(optionItem);
+	}
+
+	return optionContainer;
+}
+
+// Create and configure an individual option item
+function createOptionItem(option) {
+	const optionItem = document.createElement('DIV');
+	optionItem.innerHTML = option.innerHTML;
+
+	optionItem.addEventListener('click', function (event) {
+		updateSelectAndSelectedDiv(this, option);
+		event.target.parentNode.previousSibling.click();
+	});
+
+	return optionItem;
+}
+
+// Update the select and selected div when an option is clicked
+function updateSelectAndSelectedDiv(optionItem, option) {
+	const selectElement =
+		optionItem.parentNode.parentNode.querySelector('select');
+	const selectedDiv = optionItem.parentNode.previousSibling;
+
+	selectElement.selectedIndex = option.index;
+	selectedDiv.innerHTML = option.innerHTML;
+
+	updateSelectedClass(optionItem);
+}
+
+// Update the "same-as-selected" class for styling
+function updateSelectedClass(clickedOptionItem) {
+	const siblings = clickedOptionItem.parentNode.children;
+	for (let i = 0; i < siblings.length; i++) {
+		siblings[i].classList.remove('same-as-selected');
+	}
+	clickedOptionItem.classList.add('same-as-selected');
+}
+
+// Toggle visibility of the option container and update styling
+function toggleOptionContainer(optionContainer, selectedDiv) {
+	optionContainer.classList.toggle('select-hide');
+	selectedDiv.classList.toggle('select-arrow-active');
+}
+
+// Close all select boxes except the clicked one
+function closeAllSelectsExcept(clickedSelectedDiv) {
+	const allOptionContainers = document.getElementsByClassName('select-items');
+	const allSelectedDivs = document.getElementsByClassName('select-selected');
+
+	for (let i = 0; i < allSelectedDivs.length; i++) {
+		if (allSelectedDivs[i] !== clickedSelectedDiv) {
+			allSelectedDivs[i].classList.remove('select-arrow-active');
 		}
 	}
-	for (i = 0; i < xl; i++) {
-		if (arrNo.indexOf(i)) {
-			x[i].classList.add('select-hide');
+
+	for (let i = 0; i < allOptionContainers.length; i++) {
+		if (allSelectedDivs[i] !== clickedSelectedDiv) {
+			allOptionContainers[i].classList.add('select-hide');
 		}
 	}
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener('click', closeAllSelect);
+
+// Close all select boxes when clicking outside
+document.addEventListener('click', function () {
+	closeAllSelectsExcept(null);
+});
+
+// Initialize custom select dropdowns on page load
+initializeCustomSelects();
+
+function clickReset() {
+	const provincias = document.querySelector('#provincias');
+	const buttonClear = document.querySelector('.button2');
+	console.log('clickReset function called');
+
+	buttonClear.addEventListener('click', () => {
+		provincias.selectedIndex = 0; // Reset the selected index
+
+		// Update the custom dropdown display
+		const customSelect = document.querySelector('.custom-select');
+		const selectedDiv = customSelect.querySelector('.select-selected');
+		selectedDiv.innerHTML = provincias.options[0].innerHTML; // Display the default option text
+	});
+}
+
+clickReset();
